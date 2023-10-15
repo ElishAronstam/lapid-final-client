ff89f4023bab9b74073602064c57677161331019
Administrator <elishevaronstam@gmail.com>
2023-10-15 11:23:06 +0300

    finish search component

---
+++ src/features/task/taskSelectors.ts
@@ -0,0 +1,19 @@
+import {RootState} from "../../App";
+import {createSelector} from "@reduxjs/toolkit";
+
+export const selectTasks = (state: RootState) => state.taskSlice.tasks;
+
+export const selectItemCount = createSelector(
+    [selectTasks],
+    (items) => items.length
+);
+
+export const selectCurrentTask = (state: RootState) => {
+    return state.taskSlice.tasks.find(
+        (task) => task.id === state.taskSlice.currentTaskId
+    );
+};
+
+export const openFormDialogBoxSelector = (state: RootState) => state.taskSlice.openFormDialogBox;
+
+export const openReadDialogBoxSelector = (state:RootState) => state.taskSlice.openReadDialogBox;
